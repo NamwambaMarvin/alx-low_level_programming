f5dce6470e4c2ba99e59a872a49af6f7bce79e0d
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-16 18:01:00 -0700

    Update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - Entry point of our main function
+ * Return: returns integer zero
+ */
+int main(void)
+{
+	char m;
+
+	int n;
+
+	long int o;
+
+	long long int x;
+
+	float p;
+
+	printf "Size of char: %d bytes(s)" sizeof(m);
+	printf "Size of int: %d bytes(s)" sizeof(n);
+	printf "Size of long int: %d bytes(s)" sizeof(o);
+	printf "Size of long long int: %d bytes(s)" sizeof(x);
+	printf "Size of float: %d bytes(s)" sizeof(p);
+	return (0);
+}
+
