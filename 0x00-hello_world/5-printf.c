c77f3c91b971f0acc05f5905a2e538d2d45da830
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-16 17:41:17 -0700

    Update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of our program
+ * printf - Prints value that we want
+ * Return: Returns a zero as told
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
