60f75cff61f6021faf4760593e74e2d04e96bea6
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-16 17:01:16 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+ *Our beautiful c function that return a string and integer
+ */
 int main(void){
 	char output[] = "Programming is like building a multilingual puzzle";
 	puts(output);
-	return 0;
+	return(0);
 }
