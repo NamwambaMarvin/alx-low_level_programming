58d932321fea54596ddd65277f52c6b50b362386
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-16 19:43:24 -0700

    Update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main - Entry point of the program
+ * Return: always 0 (SUCCESS)
+ */
+int main(void)
+{
+	fprintf(stderr,
+		"%s",
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
