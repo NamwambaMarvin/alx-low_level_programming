3cb9f1bd00a47ffa0e5e3250de26abbb5f17247d
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-17 19:42:44 -0700

    c varables and conditions)

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point of the program
+ * Return:  Final value returned by the funcction
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = 0; comb < 10; ++comb)
+	{
+		putchar(comb + '0');
+		putchar(' ');
+		if (comb == 9)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(',');
+		};
+	};
+	putchar('\n');
+	return (0);
+}
