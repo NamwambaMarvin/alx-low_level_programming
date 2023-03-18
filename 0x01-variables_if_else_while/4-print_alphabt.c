dc3114e85ff6c63060f923af0619e93ff7be9381
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-17 17:48:55 -0700

    c varables and conditions)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the function
+ * Return: returns an integer
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha = 'z'; ++alpha)
+	{
+		if (alpha == 'e')
+		{
+			continue;
+		}
+		else if (alpha == 'q')
+		{
+			continue;
+		};
+		putchar(alpha);
+		return (0);
+	}
+}
