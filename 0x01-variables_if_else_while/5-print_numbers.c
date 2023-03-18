24fd30ad4210111f0d506d40530810e8906950ad
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-17 18:04:27 -0700

    c varables and conditions)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return: Returns final value of thr function
+ */
+int main(void)
+{
+	int node;
+
+	for (node = 0; node < 10; ++node)
+	{
+		printf("%d", node);
+	};
+	printf("\n");
+	return (0);
+}
