0821ff1c85cffd172d1e722cc6c725d2948ef182
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-19 09:56:39 -0700

    c varables and conditions)

---
+++ 0x01-variables_if_else_while/102-print_com5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: always zero
+ */
+int main(void)
+{
+	int Fcomb, Scomb;
+
+	for (Fcomb = 0; Fcomb < 99; ++Fcomb)
+	{
+		for (Scomb = Fcomb + 1; Scomb < 100; ++Scomb)
+		{
+			putchar((Fcomb / 10) + '0');
+			putchar((Fcomb % 10) + '0');
+			putchar(',');
+			putchar(' ');
+			putchar((Scomb / 10) + '0');
+			putchar((Scomb % 10) + '0');
+			putchar(' ');
+		};
+	};
+	putchar('\n');
+};
