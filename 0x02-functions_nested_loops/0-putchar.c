ca391fe2da59fbacb227d1dfcf6aec7b49607710
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-21 14:52:58 -0700

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * main - entry point of the program
  * Return: - Always 0 (SUCCESS)
  */
-int _putchar(char c);
 int main(void)
 {
 	char mycharacter[] = "_putchar\n";
@@ -17,15 +16,4 @@ int main(void)
 	}
 
 	return (0);
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
