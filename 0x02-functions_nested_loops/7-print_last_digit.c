dc2beb489cb679545fccc85cf7b2c8452e9adcad
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-21 18:18:48 -0700

    Update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * print_last_digit - prints the last igit if a function
  * @value_under_test: value that is parsed into the function
@@ -8,7 +9,8 @@ int print_last_digit(int value_under_test)
 {
 	int lastdigit;
 
-	lastdigit = value_under_test % 10;
+	lastdigit = (abs(value_under_test) % 10);
 
+	_putchar(lastdigit);
 	return (lastdigit);
 }
