598c3c4c23a21ae3e80edd041bd7fa53b13e4f1e
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-22 05:59:43 -0700

    Update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,13 @@ void times_table(void)
 {
 	int multiplier = 0;
 
-	int i, j, holder;
+	int i, j;
 
 	for (i = 0; i < 10; ++i)
 	{
 		for (j = 0; j < 10; ++j)
 		{
-			holder = multiplier * j;
-			_putchar('0' + holder);
+			_putchar(multiplier * j + '0');
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
