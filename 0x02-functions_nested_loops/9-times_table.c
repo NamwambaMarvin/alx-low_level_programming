3dad37c562bc48ca8fc7f172dedde5d64314f14a
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-22 10:27:40 -0700

    Update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,13 +16,21 @@ void times_table(void)
 			holder = multiplier * j;
 			if (holder > 9)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((holder / 10) + '0');
 				_putchar((holder % 10) + '0');
+				if (j == 9)
+				{
+					continue;
+				}
+				_putchar(',');
+				_putchar(' ');
 			} else
 			{
 				_putchar(holder + '0');
+				if (j == 9)
+				{
+					continue;
+				}
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
