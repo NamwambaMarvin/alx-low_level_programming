c2906a98fca803adfd766920adb3f4f0fe03fc74
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-22 17:08:32 -0700

    Update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,19 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n < 98)
+		for (; n < 98; n++)
 		{
 			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
-			++n;
 		}
 	} else
 	{
-		while (n > 98)
+		for (; n > 98; n--)
 		{
 			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
-			--n;
 		}
 	}
 }
