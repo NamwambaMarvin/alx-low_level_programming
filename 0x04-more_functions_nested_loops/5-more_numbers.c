cbf4f3580295b44010fb91936c624254e49df4e4
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-23 12:43:25 -0700

    0x04, C - More functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Function to print up to 14
+ */
+void more_numbers(void)
+{
+	int px10, upto14;
+
+	for (px10 = 0; px10 < 11; px10++)
+	{
+		for (upto14 = 0; upto14 < 15; upto14++)
+		{
+			if (upto14 > 9)
+			{
+				_putchar((upto14 / 10) + '0');
+			}
+			_putchar((upto14 % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -34,3 +34,9 @@ void print_numbers(void);
 void print_most_numbers(void);
 
 #endif
+#ifndef _MORENUMBERS_
+#define _MORENUMBERS_
+
+void more_numbers(void);
+
+#endif
