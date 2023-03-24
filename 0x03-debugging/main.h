5eb88af850aa61d1d8c2130d568c396b09851835
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-24 09:27:53 -0700

    0x03, C - Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,3 +12,9 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
 #endif
+#ifndef _LEAPYEAR_
+#define _LEAPYEAR_
+
+void print_remaining_days(int month, int day, int year);
+
+#endif
