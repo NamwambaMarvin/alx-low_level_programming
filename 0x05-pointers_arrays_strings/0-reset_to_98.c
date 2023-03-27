58c6cb04ba51c3eced7fc8ddf2b38c9492b38af5
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-27 15:10:27 -0700

    Pointers and Arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - convert the function parameters to 98
+ * @n: parameter to be converted
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef _PUTCHAR_
+#define _PUTCHAR_
+
+int _putchar(char c);
+
+#endif
+#ifndef _RESET_
+#define _RESET_
+
+void reset_to_98(int *n);
+
+#endif
