8d957db9b9117bee7843c121be3990406e92810c
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-27 16:28:11 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,9 @@ void print_rev(char *s)
 {
 	size_t length;
 
-	for (length = strlen(s); length >= 0; --length)
+	for (length = strlen(s); length >= 0; length--)
 	{
 		_putchar(s[length]);
 	}
+	_putchar('\n');
 }
