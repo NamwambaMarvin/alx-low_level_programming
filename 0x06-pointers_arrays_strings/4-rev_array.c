48fd85d0bd57d911e935067802a1b98e5e130c3f
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-30 20:10:00 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,11 @@ void reverse_array(int *a, int n)
 	int index, reve;
 
 	n = n - 1;
-
 	reve = 0;
-
 	while (reve <= n)
 	{
 		index = a[reve];
 		a[reve++] = a[n];
-		a[n--] = temp;
+		a[n--] = index;
 	}
 }
