6c4aa9ad05b4fee01d748d7a21e511ce530df8d8
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-03-31 16:31:13 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - Capitalises each word of the word
+ * @str: String whose words are to be capitalised
+ * Return: Character with capitalise words
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j = 0;
+
+	char *c = {' ',',',';','.','!','?','"','(',')','{','}'}
+
+	int array_length = sizeof(t) / sizeof(t[0]);
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= 32;
+	}
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < array_length; j++)
+		{
+			if (str[i] == c[j] || str[i] = '\t' || str[i] == '\n')
+			{
+				str[i + 1] -= 32;
+			}
+		}
+	}
+}
