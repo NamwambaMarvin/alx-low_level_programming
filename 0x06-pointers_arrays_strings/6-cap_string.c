455e29d06c99e71164f8f4592945bc2eb68aecbc
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-01 14:54:45 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,25 @@ char *cap_string(char *str)
 {
 	int i = 0, j = 0;
 
-	char *c[] = {' ',',',';','.','!','?','"','(',')','{','}'};
+	char c[] = {' ', ',', ';', '.', '!', '?', '"', '(', ')',
+		'{', '}', '\n', '\t'};
 
 	int array_length = sizeof(c) / sizeof(c[0]);
-
-	if (str[0] >= 'a' && str[0] <= 'z')
-	{
-		str[0] -= 32;
-	}
 
 	while (str[i] != '\0')
 	{
 		for (j = 0; j < array_length; j++)
 		{
-			if (str[i] == c[j] || str[i] = '\t' || str[i] == '\n')
+			if (i == 0 && (str[0] >= 'a' && str[0] <= 'z'))
+			{
+				str[i] -= 32;
+			}
+			else if (str[i] == c[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 			{
 				str[i + 1] -= 32;
 			}
 		}
+		i++;
 	}
+	return (str);
 }
