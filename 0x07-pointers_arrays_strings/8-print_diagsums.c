c093b6026591f484c7bdb087e03fe7e1b93e60e4
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-04 15:57:04 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagsums - Prints sums of square diagonals of integers
+ * @a: Array to be passed
+ * @size: Size of the square diagonals
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, total = 0, total_b = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		total += a[i][i];
+	}
+	_putchar(total + '0');
+	_putchar(',');
+	_putchar(' ');
+	for (j = size; j <= size; j--)
+	{
+		total_b += a[j][j];
+	}
+	_putchar(total_b + '0');
+}
