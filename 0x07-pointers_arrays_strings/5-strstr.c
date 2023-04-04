7a7c81d1397cce85c7b36edd6fffd1f3537d1b15
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-03 19:37:24 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * _strstr - Finds a sun string in the haystack
+ * @haystack: String under test
+ * @needle: What we are looking for
+ * Return: Substring or null
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	strstr(*haystack, *needle);
+	strstr(haystack, needle);
 }
