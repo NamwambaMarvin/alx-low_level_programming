7c64a6bde2c6f9c02ffeb492c9c890df06cecad3
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-03 18:37:05 -0700

    2-strchr.c_arrays

--- 0x07-pointers_arrays_strings/3-stspn.c
+++ 0x07-pointers_arrays_strings/3-stspn.c
@@ -1,4 +1,21 @@
 #include "main.h"
 /**
  *_strspn - Finds the first occurence of a character
- *
+ *@s: - Sting to be passed to the function
+ *@accept: - Sub string to be passed for evaluation
+ *Return: Retuns number of bytes in the initial segment s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int length_of_sub = 0;
+
+	if ((s == NULL) || (accept == NULL))
+	{
+		return (length_of_sub);
+	}
+	while (*s && strchr(accept, s++))
+	{
+		length_of_sub++;
+	}
+	return(lenth_of_sub);
+}
