cdf28ef2e2c3cdb648e3b8edb7c0fa633f253ce7
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-05 16:47:11 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_strlen_recursion - Returns legth of a string using recursion
+ *@s: String whose length is to be determined
+ *Return: Length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return *s ? 1 + _strlen_recursion(s + 1) : 0;
+}
