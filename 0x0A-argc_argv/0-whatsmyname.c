fa3f370f14914f1aeb1d074062d873fbbb264ae9
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-09 15:40:00 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints the name of the program
+ * @:argc - Number of arguments passed
+ * @:argv - Array of strings as arguments
+ * Return: - Always 0 SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef _PUTCHAR_
+#define _PUTCHAR_
+
+int _putchar(char c);
+
+#endif
