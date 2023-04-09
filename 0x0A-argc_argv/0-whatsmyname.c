5c14fdf76b0adc63249d0aaeff64e4230625234f
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-09 15:48:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @:argv - Array of strings as arguments
  * Return: - Always 0 SUCCESS
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
