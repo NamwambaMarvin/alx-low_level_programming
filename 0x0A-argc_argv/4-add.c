26ff85205a6d01c4bb6d8a50ffa5e97aaa10f236
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-10 08:56:42 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Adds two postive integes
+ * @argc: Number of arguments
+ * @argv: Ar`guments passed to the program
+ * Return: Zero on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int result, i = 0;
+
+	while (argc >= i)
+	{
+		if ((*argv[i] > 'a' && *argv[i] < 'z') || (*argv[i] > 'A' && *argv[i] < 'Z'))
+		{
+			printf("Error\n");
+			break;
+			return (1);
+		}
+		else
+		{
+			result += abs(atoi(argv[i]));
+		}
+		i++;
+		if (i == argc)
+		{
+			printf("%d\n", result);
+			return (0);
+		}
+	}
+	return (0);
+}
