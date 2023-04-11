9b2e37005ef4c9762e8c77632fd65a76e61ed6c8
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-10 18:38:59 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,11 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int total_coins = 0;
-	
-	int i, cents;
-
-	int array_of_coins[] = {25, 10, 5, 2, 1};
+	int total_coins = 0, i, cents, array_of_coins[] = {25, 10, 5, 2, 1};
 
 	int len = sizeof(array_of_coins) / sizeof(array_of_coins[0]);
 
@@ -22,9 +18,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
-
 	if (atoi(argv[1]) < 0)
 	{
 		printf("0\n");
@@ -40,10 +34,8 @@ int main(int argc, char *argv[])
 			printf("%d\n", total_coins);
 			break;
 			return (0);
-		}else
-		{
-			cents %= array_of_coins[i];
 		}
+		cents %= array_of_coins[i];
 		if (cents == 1)
 		{
 			total_coins += 1;
