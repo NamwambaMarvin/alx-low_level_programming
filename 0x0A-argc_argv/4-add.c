3e0f07be35292f6cd327f7b6372d1ec8415fca43
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-10 18:47:48 -0700

    4-add.c-update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,13 +5,13 @@
  * main - Adds two postive integes
  * @argc: Number of arguments
  * @argv: Arguments passed to the program
- * Return: Zero on success, 1 on failure
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
 	int result = 0, i = 1;
 
-	if (argc == 1)
+	if (atoi(argc) == 1)
 	{
 		printf("0\n");
 		return (1);
