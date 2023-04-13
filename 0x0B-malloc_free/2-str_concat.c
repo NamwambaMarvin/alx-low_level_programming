48204c7ebff02042a3843197842f0310edb3cb58
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-12 18:38:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@ char *str_concat(char *s1, char *s2)
 
 	unsigned int index, total_len;
 
-	if (*s1 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
-	} else if (*s2 == NULL)
+	} else if (s2 == NULL)
 	{
 		s2 = "";
 	}
