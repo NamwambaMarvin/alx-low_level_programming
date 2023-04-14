8fbff234ca0bd51f6d8b5d56c63dbb7a86adeaf1
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-13 17:23:14 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of byters of sting s2
+ * Return: Cancatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int index, lens1, lens2, total_len;
+
+	char *newstring;
+
+	lens1 = strlen(s1);
+	lens2 = strlen(s2);
+	total_len = lens1 + lens2;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	} else if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	newstring = malloc(sizeof(char)*(lens1 + lens2) + 1);
+	if (!newstring)
+	{
+		return (NULL);
+	}
+	for (index = 0; index <= total_len ; index++)
+	{
+		newstring[index] = s1[index];
+		if (index == lens1)
+		{
+			int i = 0;
+			if (n >= lens2)
+			{
+				while (s2[i] != '\0')
+				{
+					newstring[index++] = s2[i++];
+				}
+				break;
+			}
+			while (n)
+			{
+				newstring[index++] = s2[i++];
+				n--;
+			}
+			break;
+		}
+	}
+	return (newstring);
+}
