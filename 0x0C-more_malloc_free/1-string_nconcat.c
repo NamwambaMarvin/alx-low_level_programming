b45d736b9ba5a238cca8dfa5a30d76d1ce070de4
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-13 17:40:08 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,31 +12,25 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int index, lens1, lens2, total_len;
-
 	char *newstring;
 
 	lens1 = strlen(s1);
 	lens2 = strlen(s2);
 	total_len = lens1 + lens2;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	newstring = malloc(sizeof(char)*(lens1 + lens2) + 1);
+	newstring = malloc(sizeof(char) * (lens1 + lens2) + 1);
 	if (!newstring)
-	{
 		return (NULL);
-	}
 	for (index = 0; index <= total_len ; index++)
 	{
 		newstring[index] = s1[index];
 		if (index == lens1)
 		{
 			int i = 0;
+
 			if (n >= lens2)
 			{
 				while (s2[i] != '\0')
