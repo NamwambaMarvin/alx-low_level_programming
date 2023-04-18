5628f9cac473d9b167e088d772602bd6325be751
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-18 14:55:08 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define __FILENAME__ (strrchr(__FILE__, '/'))
 /**
  * main - Prints the name of the file it was compiled from
  * Return: 0 ALWAYS SUCCESS
