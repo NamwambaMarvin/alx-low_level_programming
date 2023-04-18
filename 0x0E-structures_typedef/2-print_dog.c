d1c62ec4a98b4dd842719d0b1999c4705e93ce0d
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-17 17:54:19 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		(*d).name ? printf("Name: %s\n",(*d).name) : printf("Name: (nil)\n");
-		(*d).age ? printf("Age: %.6f\n",(*d).age) : printf("Age: (nil)\n");
-		(*d).owner ? printf("Owner: %s\n",(*d).owner) : printf("Owner: (nil)\n");
+		(*d).name ? printf("Name: %s\n", (*d).name) : printf("Name: (nil)\n");
+		(*d).age ? printf("Age: %.6f\n", (*d).age) : printf("Age: (nil)\n");
+		(*d).owner ? printf("Owner: %s\n", (*d).owner) : printf("Owner: (nil)\n");
 	}
 }
