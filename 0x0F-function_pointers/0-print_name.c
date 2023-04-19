06666361362c2d5dd272350b44fc324d47cd77a6
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-19 12:15:56 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - Write a function that prints a name.
+ * @name: Name of the person
+ * @f: Function that prints the name
  */
 void print_name(char *name, void (*f)(char *))
 {
