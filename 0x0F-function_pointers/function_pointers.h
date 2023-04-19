50d2c5ef9eb08b5f62d60601749e499bb70346f6
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-19 15:24:52 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,11 @@
 #ifndef _PRINT_
 #define _PRINT_
 void print_name(char *name, void (*f)(char*));
-#endif/*
+#endif
 #ifndef _ARRAYIT__
 #define _ARRAYIT__
 void array_iterator(int *array, size_t size, void (*action)(int));
-#endif
+#endif/*
 #ifndef _PUTCHAR_
 #define _PUTCHAR_
 int _putchar(char c);
