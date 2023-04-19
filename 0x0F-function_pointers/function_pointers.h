1a68da046e762cd09d05e8951b41582ee0125fd3
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-19 11:44:44 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef _PRINT_
 #define _PRINT_
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char*));
+#endif
+#ifndef _ARRAYIT__
+#define _ARRAYIT__
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
