d33af39ef2e759b96fd2c2103a3919210a86cb01
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-20 16:15:58 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,11 +6,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list numbers;
+	unsigned int i;
+	int total = 0;
+
 	if (n == 0)
 		return (0);
-	va_list numbers;
-	int i, total = 0;
-
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
