18016ca986a4ff0a6f672e5be3e101c03c3e69cf
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-21 06:19:22 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _realloc - Implementations of realloc
+ * @ptr: Pointer to the old memory
+ * @old_size: Size of old allocated data
+ * @new_size: Size of newly allocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (!new_size)
+	{
+		free(ptr);
+		return (NULL);
+	} else if (!ptr)
+	{
+		return (malloc(new_size));
+	} else if (new_size <= old_size)
+	{
+		return (ptr);
+	} else
+	{
+		assert((ptr) && (new_size > old_size));
+		void *new_pointer = malloc(new_size);
+		if (new_pointer)
+		{
+			memcpy(new_pointer, ptr, old_size);
+			free(ptr);
+		}
+		return (new_pointer);
+	}
+}
