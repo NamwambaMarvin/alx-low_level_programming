d537409996b4fd395ad121d5267323d20f2cb1a2
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-27 17:43:10 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_len - Prints a linked list
+ * list_len - Prints a linked list
  * @h: Linked list to be printed
  * Return: Number of nodes
  */
-size_t print_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	const list_t *p = h;
 	unsigned int number = 0;
