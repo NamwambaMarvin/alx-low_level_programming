78a6870bc2c84f2df95a6670ac325bfe3fa6b143
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-04-27 19:36:15 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 /**
  * free_list - Frees memory allocate for the list
- * @list_t - List to be freed
+ * @head: - List to be freed
  */
 void free_list(list_t *head)
 {
 	list_t *ptr;
+
 	while (head)
 	{
 		ptr = head;
