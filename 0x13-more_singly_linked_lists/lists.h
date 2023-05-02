c3a66f0ccfe37f75c94a87bcff1e78ca9ba14929
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-02 14:42:52 -0700

    Betty fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,18 +2,23 @@
 #define _All
 #include <stdlib.h>
 #include <stdio.h>
-int _putchar(char c);
-/*
+/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+#endif
+#ifndef _prototypes
+#define _prototypes
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
