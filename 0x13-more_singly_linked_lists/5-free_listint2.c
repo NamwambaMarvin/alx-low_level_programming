a55bc9fd81590c584d9dfc659af2a2cbe2492c32
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-02 16:19:44 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *fr;
-
-	while (*head)
+	if (head == NULL)
 	{
-		fr = *head;
-		*head = (*head)->next;
-		free(fr);
+		while (*head)
+		{
+			fr = *head;
+			*head = (*head)->next;
+			free(fr);
+		}
 	}
 	free(*head);
 	head = NULL;
