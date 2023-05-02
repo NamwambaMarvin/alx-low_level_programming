1feae4527a8e464137222d979db3b049e8ef1707
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-02 14:46:29 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len - Display the length of the list
+ * @h: Singly linked list to be printed
+ * Return: Nuumber of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int num = 0;
+
+	while (h->next)
+	{
+		num++;
+	}
+	return (num);
+}
