5efb9cd6cf0ea893df975446d73156ad87e812d2
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-02 17:36:27 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_nodeint_at_index - Returns the nth noe of a singly linked list
+ * @head: Head of the linked list
+ * @index: Index of the node
+ * Return: nth node of the liked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int k = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head)
+	{
+		if (index == k)
+		{
+			return (k);
+		}
+		head = head->next;
+		count++
+	}
+	return (NULL);
+}
