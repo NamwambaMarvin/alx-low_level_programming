f65a35979c33e3d713956bacc64e61bdcbda872f
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-04 18:51:05 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: The number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int cover = (unsigned long int)1 << (sizeof(unsigned long int)*8 - 1);
+	int bit = 0;
+
+	while (cover != 0)
+	{
+		bit = (n & mask) == 0 ? 0 : 1;
+		_putchar(bit + '0');
+		cover >>= 1;
+	}
+}
