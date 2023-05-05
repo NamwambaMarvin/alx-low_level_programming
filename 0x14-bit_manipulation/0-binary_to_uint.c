65b30b15b93572aa546133133f0fc18de5e98afa
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-05-04 18:17:51 -0700

    betty-fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,28 @@
 #include "main.h"
+/**
+ * binary_to_uint - Converts binatry string to int
+ * @b: Binary string to be converted
+ * Return: unsigned int else 0
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uint;
 
+	if (!b)
+		return (0);
 	for (uint = 0; *b; b++)
 	{
 		if (*b == '1')
 		{
 			uint = (uint << 1) | 1;
-		} else if (uint == '0')
+		}
+		else if (uint == '0')
 		{
 			uint = uint << 1;
-		} else
+		}
+		else
 		{
-			break;
+			return (0);
 		}
 	}
 	return (uint);
