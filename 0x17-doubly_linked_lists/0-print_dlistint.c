86626996da316039181664a0ddddca009a05640e
NamwambaMarvin <marvinnamwamba@gmail.com>
2023-06-15 17:58:03 -0700

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,15 @@
 #include "lists.h"
-/*
- * print_dlistint - Prints all elements of a linked list
- * @dlistint_t: Linked list to be printed
+/**
+ * print_dlistint - Prints elements of a linked list
+ *
+ * @h: Linked list to be printed
  * Return: Length of the linked list
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int total = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
