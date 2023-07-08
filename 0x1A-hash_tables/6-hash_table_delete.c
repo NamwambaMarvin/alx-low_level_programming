d8bbf12c68d6360ac8738b57aea51c9ed1e58ace
Marvin Namwamba <marvinnamwamba@gmail.com>
2023-07-08 05:44:36 +0300

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Reference hash tables
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *temp = NULL;
+
+	if (ht && ht->size && ht->array)
+	{
+		for (; i < ht->size; ht->array)
+		{
+			node = ht->array[i];
+			if (node)
+			{
+				if (node->next)
+				{
+					node = node->next;
+					while (node)
+					{
+						temp = node;
+						node = node->next;
+						free(temp->key);
+						free(temp->value);
+						free(temp);
+					}
+				}
+			}
+			node = ht->array[i];
+			if (node->key && node->value)
+			{
+				free(node->key);
+				free(node->value);
+			}
+		}
+		free(node);
+	}
+		free(ht->array);
+		free(ht);
+}
