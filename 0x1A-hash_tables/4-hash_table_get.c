3cdad86a40ec9d7e644098c7f8bd06465a1d847f
Marvin Namwamba <marvinnamwamba@gmail.com>
2023-07-08 04:37:28 +0300

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,9 +1,15 @@
 #include <string.h>
 #include "hash_tables.h"
+/**
+ * hash_table_get - Get a value for a key
+ * @ht: Reference hash table
+ * @key: Key whose value is to be gotten
+ * Return: The value
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i = 0;
-	hash_node_t *hd= NULL;
+	hash_node_t *hd = NULL;
 
 	if (!ht || !key)
 		return (NULL);
